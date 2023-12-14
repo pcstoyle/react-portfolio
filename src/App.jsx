4c66c0027dbe9ad0e8e7a53b2bb07fbee139f42c
pcstoyle <pcstoyle@hotmail.com>
2023-12-14 16:42:16 -0700

    updated file paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
-import "./styles/about.css"
 import './styles/contact.css'
 import './styles/header.css'
 import './styles/footer.css'
